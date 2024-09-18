fc23aa2f364d945499d4641bad5492fe16f496b5
poohtankina <167339023+poohtankina@users.noreply.github.com>
2024-09-19 07:42:45 +0800

    inventory add and view

--- frontend/src/pages/inventory.jsx
+++ frontend/src/pages/inventory.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react";
+import { ref, onValue } from "firebase/database";
+import { database } from "../dbconfig/db";
 import { Link } from "react-router-dom";
 
 function inventory() {
+  const [inventoryList, setInventoryList] = useState([]);
+
+  const inventoryCollection = ref(database, "inventory");
+
+  useEffect(() => {
+    const unsubscribe = onValue(inventoryCollection, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const inventoryData = Object.keys(data).map((key) => ({
+          ...data[key],
+          id: key,
+        }));
+        setInventoryList(inventoryData);
+      } else {
+        setInventoryList([]);
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [inventoryCollection]);
+
   return (
     <div>
       <h2>inventory System</h2>
@@ -19,7 +44,23 @@ function inventory() {
             <th>Action</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {/* Check if inventoryList exists and has items */}
+          {inventoryList.length > 0 ? (
+            inventoryList.map((inventory) => (
+              <tr key={inventory.id}>
+                <td>{inventory.itemName}</td>
+                <td>{inventory.quantity}</td>
+                <td>{inventory.department}</td>
+                <td>{inventory.status}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5">No items in inventory</td>
+            </tr>
+          )}
+        </tbody>
       </table>
     </div>
   );
