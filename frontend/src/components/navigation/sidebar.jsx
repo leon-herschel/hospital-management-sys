b3306c919c17e4bc5ceac52923150c620625a707
poohtankina <167339023+poohtankina@users.noreply.github.com>
2024-09-23 23:05:32 +0800

    New branch (#21)
    * main with patient and inventory
    
    * updated patient and inventory
    
    * gawas na ang gitignore
    
    * Remove node

--- frontend/src/components/navigation/sidebar.jsx
+++ frontend/src/components/navigation/sidebar.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react';
-import logoSidebar from '../../assets/logoHeader.png';
-import { useNavigate, Link, useLocation } from 'react-router-dom';
-import { doSignOut } from '../../firebase/auth';
-import { HomeIcon, UserIcon, ClipboardDocumentListIcon, ChartBarIcon, PowerIcon, Bars3Icon, Cog8ToothIcon, UserGroupIcon } from '@heroicons/react/16/solid';
-import { Outlet } from 'react-router-dom';
+import { useState } from "react";
+import logoSidebar from "../../assets/logoHeader.png";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import { doSignOut } from "../../firebase/auth";
+import {
+  HomeIcon,
+  UserIcon,
+  ClipboardDocumentListIcon,
+  ChartBarIcon,
+  PowerIcon,
+  Bars3Icon,
+  Cog8ToothIcon,
+  UserGroupIcon,
+} from "@heroicons/react/16/solid";
+import { Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -11,54 +20,85 @@ const Sidebar = () => {
   const location = useLocation();
 
   const titles = {
-    '/dashboard': 'Overview',
-    '/patients': 'Patients',
-    '/inventory': 'Inventory',
-    '/settings': 'Settings',
-    '/analytics': 'Analytics',
+    "/dashboard": "Overview",
+    "/patients": "Patients",
+    "/inventory": "Inventory",
+    "/settings": "Settings",
+    "/analytics": "Analytics",
   };
 
-  const currentTitle = titles[location.pathname] || 'Overview';
+  const currentTitle = titles[location.pathname] || "Overview";
 
   return (
     <div className="flex h-screen shadow-lg">
       {/* Sidebar Toggle */}
-      <div className={`fixed inset-0 z-20 transition-opacity bg-neutral-100 opacity-50 lg:hidden ${sidebarOpen ? 'block' : 'hidden'}`} onClick={() => setSidebarOpen(false)}></div>
+      <div
+        className={`fixed inset-0 z-20 transition-opacity bg-neutral-100 opacity-50 lg:hidden ${
+          sidebarOpen ? "block" : "hidden"
+        }`}
+        onClick={() => setSidebarOpen(false)}
+      ></div>
 
       {/* Sidebar */}
-      <div className={`fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-red-900 ${sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'} lg:translate-x-0 lg:static lg:inset-0`}>
+      <div
+        className={`fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-red-900 ${
+          sidebarOpen ? "translate-x-0 ease-out" : "-translate-x-full ease-in"
+        } lg:translate-x-0 lg:static lg:inset-0`}
+      >
         <div className="flex items-center justify-center">
           <img src={logoSidebar} alt="Logo" className="w-auto h-30" />
         </div>
 
         {/* Navigation */}
         <nav className="mt-1">
-          <Link to="/dashboard" className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800">
+          <Link
+            to="/dashboard"
+            className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800"
+          >
             <HomeIcon className="w-6 h-6 mr-3" />
             Overview
           </Link>
 
-          <Link to="/inventory" className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800">
+          <Link
+            to="/inventory"
+            className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800"
+          >
             <ClipboardDocumentListIcon className="w-6 h-6 mr-3" />
             Inventory
           </Link>
 
-          <Link to="/patients" className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800">
+          <Link
+            to="/patients"
+            className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800"
+          >
             <UserGroupIcon className="w-6 h-6 mr-3" />
             Patients
           </Link>
 
-          <Link to="/analytics" className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800">
+          <Link
+            to="/analytics"
+            className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800"
+          >
             <ChartBarIcon className="w-6 h-6 mr-3" />
             Analytics
           </Link>
 
-          <Link to="/settings" className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800">
+          <Link
+            to="/settings"
+            className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800"
+          >
             <Cog8ToothIcon className="w-6 h-6 mr-3" />
             Settings
           </Link>
 
-          <a onClick={() => { doSignOut().then(() => { navigate('/signin') }) }} className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800 cursor-pointer">
+          <a
+            onClick={() => {
+              doSignOut().then(() => {
+                navigate("/signin");
+              });
+            }}
+            className="flex items-center px-4 py-2 mt-2 text-white opacity-85 hover:opacity-100 hover:bg-red-800 cursor-pointer"
+          >
             <PowerIcon className="w-6 h-6 mr-3" />
             Logout
           </a>
@@ -68,24 +108,27 @@ const Sidebar = () => {
       {/* Right Content */}
       <div className="flex-1 flex flex-col">
         {/* Navbar */}
-        <header className="flex justify-between items-center p-4 bg-neutral-600">
-          <button className="lg:hidden text-white" onClick={() => setSidebarOpen(true)}>
+        <header className="flex justify-between items-center p-4 bg-red-900">
+          <button
+            className="lg:hidden text-white"
+            onClick={() => setSidebarOpen(true)}
+          >
             <Bars3Icon className="w-6 h-6" />
           </button>
           <h1 className="text-2xl text-white">{currentTitle}</h1>
           <div className="flex items-center space-x-2">
-            <span className="text-white">Admin</span> 
-            <UserIcon className="w-6 h-6 text-white" /> 
+            <span className="text-white">Admin</span>
+            <UserIcon className="w-6 h-6 text-white" />
           </div>
         </header>
 
         {/* Pages Area */}
         <div className="flex-grow p-4 bg-neutral-100">
-          <Outlet />  
+          <Outlet />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
--- frontend/src/pages/Patients/ViewPatient.jsx
+++ frontend/src/pages/Patients/ViewPatient.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from "react";
 import { database } from "../../firebase/firebase";
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, push, set } from "firebase/database";
 
 function ViewPatient({ isOpen, toggleModal, patientId }) {
   const [patient, setPatient] = useState(null);
+  const [addPrescription, setAddPrescription] = useState(false);
+  const [prescriptionName, setPrescriptionName] = useState("");
+  const [dosage, setDosage] = useState("");
+  const [instruction, setInstruction] = useState("");
+  const [prescriptionList, setPrescriptionList] = useState([]);
 
   useEffect(() => {
     if (patientId) {
@@ -11,6 +16,14 @@ function ViewPatient({ isOpen, toggleModal, patientId }) {
       const unsubscribe = onValue(patientRef, (snapshot) => {
         const data = snapshot.val();
         setPatient(data || null);
+        if (data && data.prescriptions) {
+          setPrescriptionList(
+            Object.keys(data.prescriptions).map((key) => ({
+              id: key,
+              ...data.prescriptions[key],
+            }))
+          );
+        }
       });
 
       return () => {
@@ -18,6 +31,34 @@ function ViewPatient({ isOpen, toggleModal, patientId }) {
       };
     }
   }, [patientId]);
+
+  const handleAddPrescriptionClick = () => {
+    setAddPrescription((prev) => !prev);
+  };
+
+  const handleSubmit = () => {
+    if (!patientId) return;
+
+    const prescriptionRef = ref(database, `patient/${patientId}/prescriptions`);
+    const newPrescriptionRef = push(prescriptionRef);
+
+    const patientPrescription = {
+      prescriptionName: prescriptionName,
+      dosage: dosage,
+      instruction: instruction,
+    };
+
+    set(newPrescriptionRef, patientPrescription)
+      .then(() => {
+        alert("Patient's prescription has been added successfully");
+        setPrescriptionName("");
+        setDosage("");
+        setInstruction("");
+      })
+      .catch((error) => {
+        alert("Error in adding prescription: ", error);
+      });
+  };
 
   if (!patient) return null;
 
@@ -35,16 +76,83 @@ function ViewPatient({ isOpen, toggleModal, patientId }) {
           <div>
             <span className="text-2xl font-bold mb-6">Diagnosis:</span>
             <br></br>
-            {patient.diagnos ? patient.diagnosis : "No diagnosis yet"}
+            {patient.diagnosis || "No diagnosis yet"}
           </div>
 
           <div>
             <span className="text-2xl font-bold mb-6">Medicine/s used:</span>
             <br></br>
-            {patient.medUse ? patient.medUse : "No medicine used yet"}
+            {patient.medUse || "No medicine used yet"}
           </div>
         </div>
-        <button onClick={toggleModal}>Close</button>
+
+        <div>
+          <button onClick={handleAddPrescriptionClick} className="mb-4">
+            {addPrescription ? "Hide Prescription Form" : "Add Prescription"}
+          </button>
+        </div>
+        {!addPrescription ? (
+          <div>
+            {prescriptionList.map((prescription) => (
+              <div key={prescription.id}>
+                <div>
+                  <span className="text-2xl font-bold mb-6">
+                    Prescription Name:
+                  </span>
+                  <span>{prescription.prescriptionName}</span>
+                </div>
+                <div>
+                  <span className="text-2xl font-bold mb-6">Dosage:</span>
+                  <span>{prescription.dosage}</span>
+                </div>
+                <div>
+                  <span className="text-2xl font-bold mb-6">Instruction:</span>
+                  <span>{prescription.instruction}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="mt-4">
+            <div className="mb-2">
+              <label className="block mb-1">Prescription Name:</label>
+              <input
+                type="text"
+                className="w-full border border-gray-300 p-2 rounded"
+                onChange={(e) => setPrescriptionName(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="block mb-1">Dosage:</label>
+              <input
+                type="text"
+                className="w-full border border-gray-300 p-2 rounded"
+                onChange={(e) => setDosage(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="block mb-1">Instruction:</label>
+              <input
+                type="text"
+                className="w-full border border-gray-300 p-2 rounded"
+                onChange={(e) => setInstruction(e.target.value)}
+              />
+            </div>
+
+            <button
+              onClick={handleSubmit}
+              className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              Save Prescription
+            </button>
+          </div>
+        )}
+
+        <button onClick={toggleModal} className="mt-4">
+          Close
+        </button>
       </div>
     </div>
   );
