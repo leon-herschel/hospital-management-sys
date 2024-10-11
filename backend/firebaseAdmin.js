fbe85f6759f2dbc0069b8e6afdada0809dee458d
poohtankina <167339023+poohtankina@users.noreply.github.com>
2024-10-11 22:37:56 +0800

    Side branch (#104)
    * updated sidebranch
    
    * minor changes to patient

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,12 +1,9 @@
-import admin from 'firebase-admin';
-import { createRequire } from 'module'; 
-
-const require = createRequire(import.meta.url);
-const serviceAccount = require('./serviceAccountKey.json');
+import admin from "firebase-admin";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://hospital-management-syst-315f8-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  databaseURL:
+    "https://hospital-management-syst-315f8-default-rtdb.asia-southeast1.firebasedatabase.app/",
 });
 
 const db = admin.database();
