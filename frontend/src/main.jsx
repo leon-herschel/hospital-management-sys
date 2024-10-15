3936847f59034af1752d63dc2e781a44166e26a0
Joshua <joshuabongz7@gmail.com>
2024-10-15 17:07:17 +0800

    UPDATAED

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import OverAllInventory from "./pages/Inventory/OverAllInventory.jsx";
-import PharmacyTransferHistory from "./pages/History/PharmacyTransferHistory.jsx";
-import ConfirmRequest from "./pages/CSR POV/ConfirmRequest.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
