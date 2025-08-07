587677b4daede4ca84a9cc642fca7ad11d37ef67
Joshua <joshuabongz7@gmail.com>
2025-08-08 03:43:37 +0800

    firebase

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -2,7 +2,7 @@ import admin from "firebase-admin";
 
 admin.initializeApp({
   databaseURL:
-    "https://hospital-management-syst-315f8-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    "https://odyssey-test-db-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
 const db = admin.database();
